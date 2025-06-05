7573fe55ef46a02a72e3fbb612d330e1d4d7b8db
Alexis HELM <128365251+Lydovw@users.noreply.github.com>
2025-06-05 12:37:24 +1000

    added design on the cart with items

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,9 @@
 import { NavLink } from "react-router";
 import Cart from "../assets/checkout.png";
+import { use } from "react";
+import { CartContext } from "../context/CartContext";
 const Header = () => {
+  const { cartCount } = use(CartContext);
   return (
     <header className="bg-blue-600 text-white p-4 flex justify-between items-center">
       <NavLink to="/" className="hover:opacity-80 transition-opacity">
@@ -8,9 +11,14 @@ const Header = () => {
       </NavLink>
       <NavLink
         to="/cart"
-        className="p-2 hover:bg-blue-700 transition-colors rounded-full"
+        className="p-2 hover:bg-blue-700 transition-colors rounded-full relative"
       >
         <img src={Cart} alt="Cart icon" className="w-6 h-6" />
+        {cartCount > 0 && (
+          <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
+            {cartCount}
+          </span>
+        )}
       </NavLink>
     </header>
   );
